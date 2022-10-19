971e34f70ec75c81548d6976f2a8f94584ddf7d9
jaybalbas <jaybalbas98@gmail.com>
2022-10-18 19:16:39 -0700

    PC commit

--- projects/typescript-intro/example.js
+++ projects/typescript-intro/example.js
@@ -1,4 +1,9 @@
 function addShipping(price, shipping) {
-    return (price + shipping);
+    return price + shipping;
 }
 console.log(addShipping(10, 5));
+function stringCounter(word) {
+    var answer = (word.length);
+    return answer;
+}
+console.log(stringCounter("Hello World"));
--- projects/typescript-intro/example.ts
+++ projects/typescript-intro/example.ts
@@ -1,5 +1,12 @@
 function addShipping(price: number, shipping: number) : number {
-    return (price + shipping);
-    // Hello
+    return price + shipping; 
 }
-console.log(addShipping(10,5));
+
+console.log(addShipping(10, 5));
+
+function stringCounter(word: string) : number {
+    let answer:number = (word.length);
+    return answer;
+}
+
+console.log(stringCounter("Hello World"));
