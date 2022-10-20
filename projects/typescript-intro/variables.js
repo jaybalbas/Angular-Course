81b07288aa49879e0bf3b663bd1dfab8ecc9a563
jaybalbas <jaybalbas98@gmail.com>
2022-10-19 22:55:45 -0700

    learned about interfaces

--- projects/typescript-intro/variables.js
+++ projects/typescript-intro/variables.js
@@ -1,3 +1,20 @@
 // string, number, boolean, null, undefined
-var myName = '';
-myNmae = 'test';
+var myName = null;
+myName = 'test';
+//Arrays
+var items = [1, 'test'];
+;
+// Objects
+var account = {
+    // // properties
+    // name: string,
+    // balance: number,
+    // status?: string, // if object doesn't have values below, add "?" after property
+    // } = {
+    name: 'Luis',
+    balance: 10
+};
+// Just using non-Interface method
+// let accounts: {}[]; // store array "[]" of objects "{}"
+// Interface method
+var accounts;
--- projects/typescript-intro/variables.ts
+++ projects/typescript-intro/variables.ts
@@ -1,3 +1,53 @@
 // string, number, boolean, null, undefined
-let myName: string = '';
-myNmae = 'test';
+let myName: string | null = null;
+myName = 'test';
+
+//Arrays
+let items: (string | number)[] = [1, 'test'];
+
+// Interface - Use Pascal naming
+// won't be compiled into JS
+interface IAccount{
+    name: string,
+    balance: number,
+    status?: string,
+    deposit?: () =>  void // method . add "?" to make method optional
+};
+
+
+// Objects
+const account: IAccount = {
+    // // properties
+    // name: string,
+    // balance: number,
+    // status?: string, // if object doesn't have values below, add "?" after property
+// } = {
+    name: 'Luis',
+    balance: 10
+};
+
+// Just using non-Interface method
+// let accounts: {}[]; // store array "[]" of objects "{}"
+
+// Interface method
+let accounts: IAccount[];
+
+
+// use "implements" if wanna use interface
+class InvestmentAccount implements IAccount {
+    // name;
+    // balance;
+
+    // We can use constructor to initialize properties
+    // Use "public" if wanna have access to properties outside of class (public by default)
+    constructor (
+        public name, public balance
+    ) {
+
+    }
+
+    // "private" can only be used within the class
+    private withdraw() {
+
+    }
+}
