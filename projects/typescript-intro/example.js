ff7f51920fa08ee1ac39981d974962d7a5f24855
jaybalbas <jaybalbas98@gmail.com>
2022-10-19 17:49:23 -0700

    tested class

--- projects/typescript-intro/example.js
+++ projects/typescript-intro/example.js
@@ -1,9 +1,39 @@
 function addShipping(price, shipping) {
     return price + shipping;
 }
-console.log(addShipping(10, 5));
+// console.log(addShipping(10, 5));
 function stringCounter(word) {
     var answer = (word.length);
     return answer;
 }
-console.log(stringCounter("Hello World"));
+// console.log(stringCounter("Hello World"));
+// for (let i = 0; i < 15; ++i) {
+//     let x = addShipping(0,i);
+//     x += 1;
+//     console.log(x);
+// }
+var Operations = /** @class */ (function () {
+    function Operations(n, m) {
+        this.num1 = n;
+        this.num2 = m;
+    }
+    Operations.prototype.adding = function () {
+        return this.num1 + this.num2;
+    };
+    Operations.prototype.subtracting = function () {
+        return this.num1 - this.num2;
+    };
+    Operations.prototype.multiplying = function () {
+        return this.num1 * this.num2;
+    };
+    Operations.prototype.dividing = function () {
+        return this.num1 / this.num2;
+    };
+    return Operations;
+}());
+;
+var hello = new Operations(20, 5);
+console.log("add: " + hello.adding());
+console.log("sub: " + hello.subtracting());
+console.log("mult: " + hello.multiplying());
+console.log("div: " + hello.dividing());
--- projects/typescript-intro/example.ts
+++ projects/typescript-intro/example.ts
@@ -2,11 +2,52 @@ function addShipping(price: number, shipping: number) : number {
     return price + shipping; 
 }
 
-console.log(addShipping(10, 5));
+// console.log(addShipping(10, 5));
 
 function stringCounter(word: string) : number {
     let answer:number = (word.length);
     return answer;
 }
 
-console.log(stringCounter("Hello World"));
+// console.log(stringCounter("Hello World"));
+
+// for (let i = 0; i < 15; ++i) {
+//     let x = addShipping(0,i);
+//     x += 1;
+//     console.log(x);
+// }
+
+class Operations {
+    num1: number;
+    num2: number;
+
+    constructor(n: number, m: number) {
+        this.num1 = n;
+        this.num2 = m;
+    }
+
+    adding() {
+        return this.num1 + this.num2;
+    }
+
+    subtracting() {
+        return this.num1 - this.num2;
+    }
+
+    multiplying() {
+        return this.num1 * this.num2;
+    }
+
+    dividing() {
+        return this.num1 / this.num2;
+    }
+
+};
+
+
+var hello = new Operations(20,5);
+
+console.log("add: " + hello.adding());
+console.log("sub: " + hello.subtracting());
+console.log("mult: " + hello.multiplying());
+console.log("div: " + hello.dividing());
